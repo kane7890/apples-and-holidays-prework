cc38a1a215b4c3a4ff52ce2834ce55f6eabb47ab
Kane Koo <kanekoo@gmail.com>
2019-03-26 21:42:16 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,6 +57,7 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
+  binding.pry
   holiday_hash.each do |season, holiday|
     season_name=season.to_s
     
