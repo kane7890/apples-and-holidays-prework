3d4b6959c437da90d9b62cbbd21a914ecad51384
Kane Koo <kanekoo@gmail.com>
2019-03-26 21:43:11 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,7 @@ def all_supplies_in_holidays(holiday_hash)
   binding.pry
   holiday_hash.each do |season, holiday|
     season_name=season.to_s
-    
+    binding.pry
     puts "#{season_name}:"
     binding.pry
     holiday.each do |holiday, supplies|
